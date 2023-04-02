095110315e8ff14e0c3d26c2ae3f4258849fe0e6
frodggy <frodggyxd@gmail.com>
2023-04-02 00:25:49 +0000

    fixed events

--- src/tag.ts
+++ src/tag.ts
@@ -73,7 +73,7 @@ const TagParser = {
     const attrs = {}
 
     tag.attrs.forEach(attr => {
-      if (attr.name.match(/^->/)) {
+      if (attr.name.match(/^when:/)) {
         listeners.push({
           index,
           event: attr.name.split(':')[1],
